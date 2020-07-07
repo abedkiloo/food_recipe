bf09761bf51acdb525866629614c9d0c538958b2
Abednego Wambua <abedxh@gmail.com>
2020-07-08 00:45:17 +0300

    test adding users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -34,3 +34,10 @@ class AdminSiteTest(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_user_page(self):
+        """Test that the create user model works"""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
